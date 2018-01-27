fea6592672bebdbb50e2d092c977eab1c1ab96f1
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-01-27 18:47:29 -0500

    %Infection,WinCondition

---
+++ GGJ 2018/Assets/Scripts/InfectedItem/InfectableItems.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectableItems : MonoBehaviour, IInfectable
+{
+
+    public float timeToItemGetInfected = 3f;
+    public int percentageOfItem = 5;
+
+    public float time = 0f;
+
+    public bool infected = false;
+
+    public void Infect(bool _infected)
+    {
+        if (infected)
+        {
+            return;
+        }
+
+        if (_infected == true)
+        {
+            time += Time.deltaTime;
+
+            if (time > timeToItemGetInfected)
+            {
+                infected = true; // Infection Complete
+                
+                PercentageOfInfection.percentage = Mathf.Min(PercentageOfInfection.percentage + percentageOfItem, 100);
+                    
+            }
+            else
+            {
+                infected = false;
+            }
+        }
+
+        
+    }
+
+    public void Uninfected()
+    {
+        time = 0f;
+    }
+}
