6151105d973d9fbe2f6e608f164a95815ca5baed
Juan David <juan.atehortual@upb.edu.co>
2018-01-27 10:12:40 -0500

    Added rat class

--- GGJ 2018/Assets/Scripts/RatMovement.cs
+++ GGJ 2018/Assets/Scripts/RatMovement.cs
@@ -2,20 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RatMovement : MonoBehaviour {
-
-	float velMovement = 3f;
-	float dirX, dirY;
-    Transform mTransform;
-	void Start () {
-        mTransform = GetComponent<Transform>();
+public class RatMovement : Rat
+{
+	void Update ()
+    {
+        Move();
 	}
 
-	void Update () {
-		float sentidoX = Input.GetAxis ("Horizontal") * velMovement;
+    void Move()
+    {
+        float sentidoX = Input.GetAxis("Horizontal") * velMovement;
         float sentidoZ = Input.GetAxis("Vertical") * velMovement;
-		mTransform.position += new Vector3(sentidoX, 0,sentidoZ) * Time.deltaTime;
-	}
-
-
+        transform.position += new Vector3(sentidoX, 0, sentidoZ) * Time.deltaTime;
+    }
 }
