fd4b056209f36b1719f9058ca1080bb4183c8e80
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-01-27 14:16:12 -0500

    InfectionItems
    %% of infection left

---
+++ GGJ 2018/Assets/Scripts/InfectedItem/FoodInfected.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodInfected : MonoBehaviour, IInfectable
+{
+
+    public float timeToFoodGetInfected = 4f;
+    private float time = 0f;
+
+    bool infected = false;
+
+    public void Infect(bool _infected)
+    {
+        if (_infected == true)
+        {
+            time += Time.deltaTime;
+
+            if (time > timeToFoodGetInfected)
+            {
+                infected = true; // Infection Complete
+            }
+        }
+    }
+
+    public void Uninfected()
+    {
+        time = 0f;
+    }
+}
