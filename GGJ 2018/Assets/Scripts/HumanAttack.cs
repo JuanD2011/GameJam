7a41278d6ce0d3343bf8f95bbb6a9a6a18db348f
Pipe98x <pipe989898@gmail.com>
2018-01-27 19:17:32 -0500

    Game over
    Inteligencia artificial lista

---
+++ GGJ 2018/Assets/Scripts/HumanAttack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanAttack : MonoBehaviour {
+
+	public GameObject gameover;
+	void OnCollisionEnter(Collision other) {
+
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.GetComponent<Rat> ().muerte=true;
+			gameover.SetActive (true);
+		}
+	}
+}
